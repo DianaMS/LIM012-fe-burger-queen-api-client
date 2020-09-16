544c398fdc196d2f69703ff0a6893a84ca711ccc
DianaMS <dmartinezs@uni.pe>
2020-09-15 19:53:45 -0500

    new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    OrdersComponent,
+    ProductsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
